f45b00fe6795252f59a0b961a899ab117a36df1e
Orban Tamas <orbant1@gmail.com>
2023-12-15 18:22:49 +0100

    github icon absolut

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import React,{useEffect} from 'react';
 import '../CSS/navbar.css';
 import {ClippifyData,LupodyData,LupodyMobileData,ClippifyMobileData} from '../portfolio.jsx'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 
 const PortfolioPage = () => {
@@ -50,10 +51,15 @@ return(
             <h3 className="this-project-title">This Project <br /> Made Me an Expert In</h3>
             {pageData.expertise.map((item, index) => {
                 return (
-                    <>
+                    <div>
+                           <div className="github-link">
+                            <GitHubIcon />
+                        </div>
                     <div className={index % 2 == 0 ? "experience-box" : "experience-box-reverse"} key={index}>
+                 
                         <img className="exp-img" src={item.logo} alt="" />
                         <div className="exp-text">
+                            
                             <h4 className="exp-title">{item.title}</h4>
                             <ul>
                                 {item.text.map((item2, index2) => {
@@ -63,11 +69,15 @@ return(
                                 })}
                             </ul> 
                         </div>
+                     
                     </div>
+                  
                     <div className="experience-show">
                         {item.show && item.show()}
+                    
                     </div>
-                </>
+                
+                </div>
                 );
             })}
         </div>
